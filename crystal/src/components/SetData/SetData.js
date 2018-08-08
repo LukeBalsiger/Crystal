f663f7d531a4f813bd79137c2bc43be2426c777a
Luke Balsiger <Luke Balsiger>
2018-08-08 09:50:58 -0500

    Same thing but with sets

--- crystal/src/components/SetData/SetData.js
+++ crystal/src/components/SetData/SetData.js
@@ -9,12 +9,16 @@ export default class SetData extends React.Component {
   constructor(props) {
     super(props)
     this.state = { 
-      value: "base1",
-      cardNameList: ["Please Wait..."],
-      urlList: []
+        showImage: false,
+        showDataToCopy: false,
+        value: "base1",
+        cardNameList: ["Please Wait..."],
+        urlList: []
     }
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleImageCheck = this.handleImageCheck.bind(this);
+    this.handleDataCheck = this.handleDataCheck.bind(this);
   }
 
   componentDidMount() {
@@ -33,6 +37,14 @@ export default class SetData extends React.Component {
                 urlList: listUrls
             })
     });
+  }
+
+  handleImageCheck(event) {
+    this.setState({ showImage: !this.state.showImage })
+  }
+
+  handleDataCheck(event) {
+    this.setState({ showDataToCopy: !this.state.showDataToCopy })
   }
 
   handleChange(event) {
@@ -69,40 +81,91 @@ export default class SetData extends React.Component {
     })
 
     var allCardsForSelected = this.state.cardNameList.map((card, index) => {
-      return <li key={card + index}><a href={urlLinkList[index]} key={urlLinkList[index] + index}>{card}</a></li>
+      return <li key={card + index}>{card}</li>
     })
 
     var allUrlsForSelected = this.state.urlList.map((url, index) => {
-      return <li key={url + index}><a href={url} key={url + index}>{url}</a></li>
+      return <li key={url + index}>{url}</li>
     })
+
+    var tableRowsData = () => {
+        var multiArray = new Array(this.state.cardNameList.length);
+        for (var i = 0; i < this.state.cardNameList.length; i++) {
+          multiArray[i] = new Array(2);
+        }
+        for(var j = 0; j < this.state.cardNameList.length; j++) {
+          multiArray[j][0] = this.state.cardNameList[j]
+          multiArray[j][1] = this.state.urlList[j]
+        }
+        return multiArray
+      }
+  
+      var tableRows = tableRowsData().map((tableRow, index) => {
+          var imageColumn = <td className="SetData-table-data"><a href={tableRow[1]} key={tableRow[1] + index}>Link</a></td>
+          if(this.state.showImage) { imageColumn = <td className="SetData-table-data"><img src={tableRow[1]} alt="Card"/></td> }
+          return (
+            <tr key={index} className="SetData-table-row">
+              <td className="SetData-table-data">{tableRow[0]}</td>
+              {imageColumn}
+              </tr>
+          )
+      })
+
+      var dataToCopyBlock = () => {
+        if(this.state.showDataToCopy) {
+          return (
+            <div className="SetData-display-block">
+                <div className="SetData-card-list-display">
+                <h4 className="SetData-h4">Set has these cards:</h4>
+                <ul className="SetData-ul">
+                    {allCardsForSelected}
+                </ul>
+                </div>
+                <div className="SetData-url-list-display">
+                <h4 className="SetData-h4">Image urls:</h4>
+                <ul className="SetData-ul">
+                    {allUrlsForSelected}
+                </ul>
+                </div>
+            </div>
+          )
+        }
+      }
 
     return (
       <div>
         <CrystalNavbar />
         <div className="SetData-component">
-          <br />
-          <div className="SetData-form-block">
-            <form>
-              <label>Select the Set you wish to see card information for:   </label>
-              <select className="SetData-select" value={this.state.value} onChange={this.handleChange}>
-                  {cmbxItems}
-              </select>
-            </form>
-          </div>
-          <div className="SetData-display-block">
-            <div className="SetData-card-list-display">
-              <h4 className="SetData-h4">Set has these cards:</h4>
-              <ul className="SetData-ul">
-                {allCardsForSelected}
-              </ul>
+            <br />
+            <div className="SetData-form-block">
+                <form>
+                <label>Select the Set you wish to see card information for:   </label>
+                <select className="SetData-select" value={this.state.value} onChange={this.handleChange}>
+                    {cmbxItems}
+                </select>
+                <br />
+                <input type="checkbox" value={this.state.showImage} onChange={this.handleImageCheck}/>
+                <label>Show images</label>
+                <input type="checkbox" value={this.state.showDataToCopy} onChange={this.handleDataCheck}/>
+                <label>Show more data</label>
+                </form>
             </div>
-            <div className="SetData-url-list-display">
-              <h4 className="SetData-h4">Image urls:</h4>
-              <ul className="SetData-ul">
-                {allUrlsForSelected}
-              </ul>
+            <div className="SetData-table-display">
+                <table className="SetData-table">
+                <thead className="SetData-table-head">
+                    <tr className="SetData-table-row">
+                    <th className="SetData-table-head-column">Card</th>
+                    <th className="SetData-table-head-column">Image</th>
+                    </tr>
+                </thead>
+                <tbody className="SetData-table-body">
+                    {tableRows}
+                </tbody>
+                </table>
             </div>
-          </div>
+            <div>
+                {dataToCopyBlock()}
+            </div>
         </div>
       </div>
     )
--- crystal/src/components/TCGData/TCGData.js
+++ crystal/src/components/TCGData/TCGData.js
@@ -8,6 +8,8 @@ export default class TCGData extends React.Component {
   constructor(props) {
     super(props)
     this.state = { 
+      showImage: false,
+      showDataToCopy: false,
       value: "Bulbasaur",
       setList: ["Please wait..."],
       numList: [],
@@ -16,6 +18,8 @@ export default class TCGData extends React.Component {
     }
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleImageCheck = this.handleImageCheck.bind(this);
+    this.handleDataCheck = this.handleDataCheck.bind(this);
   }
 
   componentDidMount() {
@@ -42,6 +46,14 @@ export default class TCGData extends React.Component {
             urlList: listUrl
           })
     });
+  }
+
+  handleImageCheck(event) {
+    this.setState({ showImage: !this.state.showImage })
+  }
+
+  handleDataCheck(event) {
+    this.setState({ showDataToCopy: !this.state.showDataToCopy })
   }
 
   handleChange(event) {
@@ -75,69 +87,125 @@ export default class TCGData extends React.Component {
   render() {
     var cmbxItems = data.names.map((name) => {
         if(name === "Bulbasaur") {
-            return <option key={name} selected value={`${name}`}>{name}</option>
+            return <option key={name} defaultValue={`${name}`}>{name}</option>
         }
         return <option key={name} value={`${name}`}>{name}</option>
     })
 
-    var urlLinkList = this.state.urlList.map(url => {
-      return url
-    })
-
     var allSetsForSelected = this.state.setList.map((set, index) => {
-      return <li key={set + index}><a href={urlLinkList[index]} key={urlLinkList[index] + index}>{set}</a></li>
+      return <li key={set + index}>{set}</li>
     })
 
     var allNumbersForSelected = this.state.numList.map((num, index) => {
-      return <li key={num + index}><a href={urlLinkList[index]} key={urlLinkList[index] + index}>{num}</a></li>
+      return <li key={num + index}>{num}</li>
     })
 
     var allRaritiesForSelected = this.state.rarityList.map((rarity, index) => {
-      return <li key={rarity + index}><a href={urlLinkList[index]} key={urlLinkList[index] + index}>{rarity}</a></li>
+      return <li key={rarity + index}>{rarity}</li>
     })
 
     var allUrlsForSelected = this.state.urlList.map((url, index) => {
-      return <li key={url + index}><a href={url} key={url + index}>{url}</a></li>
+      return <li key={url + index}>{url}</li>
     })
+
+    var tableRowsData = () => {
+      var multiArray = new Array(this.state.setList.length);
+      for (var i = 0; i < this.state.setList.length; i++) {
+        multiArray[i] = new Array(4);
+      }
+      for(var j = 0; j < this.state.setList.length; j++) {
+        multiArray[j][0] = this.state.setList[j]
+        multiArray[j][1] = this.state.numList[j]
+        multiArray[j][2] = this.state.rarityList[j]
+        multiArray[j][3] = this.state.urlList[j]
+      }
+      return multiArray
+    }
+
+    var tableRows = tableRowsData().map((tableRow, index) => {
+        var imageColumn = <td className="TCGData-table-data"><a href={tableRow[3]} key={tableRow[3] + index}>Link</a></td>
+        if(this.state.showImage) { imageColumn = <td className="TCGData-table-data"><img src={tableRow[3]} alt="Card"/></td> }
+        return (
+          <tr key={index} className="TCGData-table-row">
+            <td className="TCGData-table-data">{tableRow[0]}</td>
+            <td className="TCGData-table-data">{tableRow[1]}</td>
+            <td className="TCGData-table-data">{tableRow[2]}</td>
+            {imageColumn}
+            </tr>
+        )
+    })
+
+    var dataToCopyBlock = () => {
+      if(this.state.showDataToCopy) {
+        return (
+          <div className="TCGData-display-block">
+              <div className="TCGData-set-list-display">
+                <h3 className="tCGData-h3">---Data to copy---</h3>
+                <h4 className="TCGData-h4">Sets</h4>
+                <ul className="TCGData-ul">
+                  {allSetsForSelected}
+                </ul>
+              </div>
+              <div className="TCGData-num-list-display">
+                <h4 className="TCGData-h4">Set Numbers</h4>
+                <ul className="TCGData-ul">
+                  {allNumbersForSelected}
+                </ul>
+              </div>
+              <div className="TCGData-rarity-list-display">
+                <h4 className="TCGData-h4">Rarities</h4>
+                <ul className="TCGData-ul">
+                  {allRaritiesForSelected}
+                </ul>
+              </div>
+              <div className="TCGData-url-list-display">
+                <h4 className="TCGData-h4">Image urls</h4>
+                <ul className="TCGData-ul">
+                  {allUrlsForSelected}
+                </ul>
+              </div>
+            </div>
+        )
+      }
+    }
 
     return (
       <div>
         <CrystalNavbar />
+        <br />
         <div className="TCGData-component">
           <br />
           <div className="TCGData-form-block">
             <form>
-              <label>Select the Pokemon you wish to see card information for:   </label>
+              <label>Select the Pokemon you wish to see card information for:   </label>              
               <select className="TCGData-select" value={this.state.value} onChange={this.handleChange}>
                   {cmbxItems}
               </select>
+              <br />
+              <input type="checkbox" value={this.state.showImage} onChange={this.handleImageCheck}/>
+              <label>Show images</label>
+              <input type="checkbox" value={this.state.showDataToCopy} onChange={this.handleDataCheck}/>
+              <label>Show more data</label>
             </form>
           </div>
-          <div className="TCGData-display-block">
-            <div className="TCGData-set-list-display">
-              <h4 className="TCGData-h4">Sets this pokemon has a card in:</h4>
-              <ul className="TCGData-ul">
-                {allSetsForSelected}
-              </ul>
-            </div>
-            <div className="TCGData-num-list-display">
-              <h4 className="TCGData-h4">Set Numbers:</h4>
-              <ul className="TCGData-ul">
-                {allNumbersForSelected}
-              </ul>
-            </div>
-            <div className="TCGData-rarity-list-display">
-              <h4 className="TCGData-h4">Rarities:</h4>
-              <ul className="TCGData-ul">
-                {allRaritiesForSelected}
-              </ul>
-            </div>
-            <div className="TCGData-url-list-display">
-              <h4 className="TCGData-h4">Image urls:</h4>
-              <ul className="TCGData-ul">
-                {allUrlsForSelected}
-              </ul>
-            </div>
+          <br />
+          <div className="TCGData-table-display">
+            <table className="TCGData-table">
+              <thead className="TCGData-table-head">
+                <tr className="TCGData-table-row">
+                  <th className="TCGData-table-head-column">Set</th>
+                  <th className="TCGData-table-head-column">Number</th> 
+                  <th className="TCGData-table-head-column">Rarity</th>
+                  <th className="TCGData-table-head-column">Image</th>
+                </tr>
+              </thead>
+              <tbody className="TCGData-table-body">
+                {tableRows}
+              </tbody>
+            </table>
+          </div>
+          <div>
+            {dataToCopyBlock()}
           </div>
         </div>
       </div>
